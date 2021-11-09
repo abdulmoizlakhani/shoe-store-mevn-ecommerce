681c6c72674818c7baf9f4dbb8e1903d93c97f44
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2021-11-09 18:19:39 +0500

    - Folder structure setup - routes setup - created products page - created product detail page - created cart page - added respective styles in each page - added required assets (product images etc)

---
+++ client/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+// Components
+import CartPage from "../views/CartPage.vue";
+import ProductDetailPage from "../views/ProductDetailPage.vue";
+import ProductsPage from "../views/ProductsPage.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/products",
+    name: "Products",
+    component: ProductsPage,
+  },
+  {
+    path: "/products/:id",
+    name: "Product Details Page",
+    component: ProductDetailPage,
+  },
+  {
+    path: "/cart",
+    name: "Cart",
+    component: CartPage,
+  },
+  {
+    path: "/",
+    redirect: "/products",
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
---
+++ client/src/views/ProductDetailPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>The Product Detail Page</h1>
+</template>
+
+<script>
+export default {
+  name: "ProductDetailPage",
+};
+</script>
+
+<style scoped>
+#page-wrap {
+  margin-top: 16px;
+  padding: 16px;
+  max-width: 600px;
+}
+
+#img-wrap {
+  text-align: center;
+}
+
+img {
+  width: 400px;
+}
+
+#product-details {
+  padding: 16px;
+  position: relative;
+}
+
+#add-to-cart {
+  width: 100%;
+}
+
+#price {
+  position: absolute;
+  top: 24px;
+  right: 16px;
+}
+
+.green-button {
+  background-color: green;
+}
+
+.grey-button {
+  background-color: #888;
+}
+</style>
