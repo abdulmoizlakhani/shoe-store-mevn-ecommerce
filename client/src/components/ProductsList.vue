b4bb882ea146010bbdd121d053231eff967ac2f5
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2021-11-09 21:15:26 +0500

    - Modularized Cart Page - created products list page - created product list item page

---
+++ client/src/components/ProductsList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="cartItems.length > 0">
+    <ProductsListItem
+      :key="product.id"
+      v-for="product in cartItems"
+      :product="product"
+    />
+  </div>
+  <p v-else>You haven't added anything to your cart yet!</p>
+</template>
+
+<script>
+import ProductsListItem from "./ProductsListItem.vue";
+
+export default {
+  name: "ProductsList",
+  components: { ProductsListItem },
+  props: ["cartItems"],
+  computed: {
+    totalPrice() {
+      return this.cartItems.reduce((sum, item) => sum + Number(item.price), 0);
+    },
+  },
+};
+</script>
