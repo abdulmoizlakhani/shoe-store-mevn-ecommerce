7383927810591a69dae00303b04de0fd7b40479c
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2021-11-09 20:44:43 +0500

    - modularized products page - created products grid - created products grid item - inserted scoped attribute on not found page style tag

---
+++ client/src/components/ProductsGrid.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="grid-wrap">
+    <ProductsGridItem
+      :key="product.id"
+      v-for="product in products"
+      :product="product"
+    />
+  </div>
+</template>
+
+<script>
+import ProductsGridItem from "./ProductsGridItem.vue";
+
+export default {
+  name: "ProductsGrid",
+  components: { ProductsGridItem },
+  props: ["products"],
+};
+</script>
+
+<style scoped>
+.grid-wrap {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 16px;
+}
+</style>
