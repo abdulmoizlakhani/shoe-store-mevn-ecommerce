b0728f047e80d6a682a64495957bb462aca327ed
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2021-11-09 19:22:31 +0500

    - created product detail page

--- client/src/views/ProductDetailPage.vue
+++ client/src/views/ProductDetailPage.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>The Product Detail Page</h1>
+  <div id="page-wrap">
+    <div id="img-wrap">
+      <img :src="product.imageUrl" alt="single product" />
+    </div>
+    <div id="product-details">
+      <h1>{{ product.name }}</h1>
+      <h3 id-="price">{{ product.price }}</h3>
+      <p>Average Rating: {{ product.averageRating }}</p>
+      <button id="add-to-cart">Add to Cart</button>
+      <h4>Description</h4>
+      <p>{{ product.description }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import { products } from "../fake-data";
+
 export default {
   name: "ProductDetailPage",
+  data() {
+    return {
+      product: products.find((p) => p.id === this.$route.params.id),
+    };
+  },
 };
 </script>
 
